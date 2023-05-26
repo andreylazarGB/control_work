e9058a7e52d62a7509bc805ab5734bd980a0d746
andrey lazar <andreylazarmr@gmail.com>
2023-05-26 09:24:44 +0300

    control work

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// 
+
+string[] array = { "hello", "2", "world", ";)" };
+
+void GetArray(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        char[] arr = Array[i].ToCharArray();
+        for (int j = 0; j < arr.Length; j++)
+        {
+            int size = 3;
+            if (arr.Length <= size)
+            {
+                //Array[i] = Array[0];
+                //Console.WriteLine($"{Array[i]}");
+                //Console.Write(arr[j]+" | ");
+            }
+            Console.WriteLine($"{String.Join(Array)}");
+        }
+        //Console.WriteLine($"{arr}");
+    }
+    //Console.WriteLine(Array);
+}
+
+GetArray(array);
